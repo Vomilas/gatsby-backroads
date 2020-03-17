f3d3c074fd9fc1f5ec3c86dab5025048827171c4
Anush Salimov <safmail96@gmail.com>
2020-03-17 22:59:43 +0300

    add siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
 module.exports = {
+  siteMetadata: {
+    title: "BackRoads",
+    description: `Explore awesome worldwide tours & 
+      discover what makes each of them unique. 
+      Forget your daily routine & say yes to adventure`,
+    author: "@vomilas",
+  },
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-styled-components"],
 }
