0d1106d98a005cd49cb57d16bed5a2f3c55e96d9
Anush Salimov <safmail96@gmail.com>
2020-03-23 20:47:42 +0300

    page numbering

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -6,16 +6,50 @@ import styles from "../css/blog.module.css"
 import BlogCard from "../components/Blog/BlogCard"
 import Title from "../components/Title"
 
-const Blog = ({ data }) => {
+const Blog = ({ data, pageContext: { currentPage, numPages } }) => {
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+
+  const prevPage = currentPage === 2 ? `/blogs/` : `/blogs/${currentPage - 1}`
+  const nextPage = `/blogs/${currentPage + 1}`
+
   return (
-    <section className={styles.blog}>
-      <Title title="latest" subtitle="posts" />
-      <div className={styles.center}>
-        {data.posts.edges.map(({ node }) => (
-          <BlogCard key={node.id} blog={node} />
-        ))}
-      </div>
-    </section>
+    <Layout>
+      <section className={styles.blog}>
+        <Title title="latest" subtitle="posts" />
+        <div className={styles.center}>
+          {data.posts.edges.map(({ node }) => (
+            <BlogCard key={node.id} blog={node} />
+          ))}
+        </div>
+        <section className={styles.links}>
+          {!isFirst && (
+            <AniLink fade to={prevPage} className={styles.link}>
+              Prev
+            </AniLink>
+          )}
+          {Array.from({ length: numPages }, (_, i) => (
+            <AniLink
+              key={i}
+              fade
+              to={`/blogs/${i === 0 ? "" : i + 1}`}
+              className={
+                i + 1 === currentPage
+                  ? `${styles.link} ${styles.active}`
+                  : `${styles.link}`
+              }
+            >
+              {i + 1}
+            </AniLink>
+          ))}
+          {!isLast && (
+            <AniLink fade to={nextPage} className={styles.link}>
+              Next
+            </AniLink>
+          )}
+        </section>
+      </section>
+    </Layout>
   )
 }
 
